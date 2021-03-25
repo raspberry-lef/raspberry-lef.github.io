7ab9fd4ad9c658c4aed4c916a21ee967a628d5a5
raspberry-lef <raspberry-lef@automaticmode.com>
2021-03-25 17:56:14 +0200

    Sidebar expanded by default

--- sidebars.js
+++ sidebars.js
@@ -1,20 +1,29 @@
 module.exports = {
-  someSidebar: {
-    ActiveWorkflow: [
-      'getting-started',
-      'use-cases',
-      'activeworkflow-basics',
-      'built-in-agents',
-      'agent-examples',
-      'remote-agent-api',
-      'rest-api',
-      'deployment',
-      'roadmap',
-    ],
-    'Helper Libraries': [
-      'activeworkflow-agent-python',
-      'activeworkflow-agent-ruby',
-    ],
-
-  },
-};
+  docs: [
+    {
+      type: 'category',
+      label: 'ActiveWorkflow',
+      collapsed: false,
+      items: [
+        'getting-started',
+        'use-cases',
+        'activeworkflow-basics',
+        'built-in-agents',
+        'agent-examples',
+        'remote-agent-api',
+        'rest-api',
+        'deployment',
+        'roadmap',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Helper Libraries',
+      collapsed: false,
+      items: [
+        'activeworkflow-agent-python',
+        'activeworkflow-agent-ruby'
+      ],
+    }
+  ]
+}
